f3c30fb52162f65c40c90e4d910e0252610d6d52
Yo Vannaravuth <ravuthz@gmail.com>
2017-11-18 17:07:53 +0000

    Add mlab mongodb

--- config/connections.js
+++ config/connections.js
@@ -56,13 +56,13 @@ module.exports.connections = {
   * Run: npm install sails-mongo                                             *
   *                                                                          *
   ***************************************************************************/
-  someMongodbServer: {
-    adapter: 'sails-mongo',
-    host: 'localhost',
-    port: 27017,
-    // user: 'username',
-    // password: 'password',
-    // database: 'your_mongo_db_name_here'
+  mlabMongoDB: {
+      adapter: "sails-mongo",
+      host: "ds113626.mlab.com",
+      port: 13626,
+      user: "ravuthz",
+      password: "ravuthz",
+      database: "sails-oauth2-api"
   },
 
   /***************************************************************************
