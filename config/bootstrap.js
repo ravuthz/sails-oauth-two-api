6f859a42fb18d70a05bed0228842cd56cc8a5760
Yo Vannaravuth <ravuthz@gmail.com>
2017-11-18 16:57:47 +0000

    Init Sails + Oauth2

---
+++ config/bootstrap.js
@@ -0,0 +1,78 @@
+/**
+ * Bootstrap
+ *
+ * An asynchronous boostrap function that runs before your Sails app gets lifted.
+ * This gives you an opportunity to set up your data model, run jobs, or perform some special logic.
+ *
+ * For more information on bootstrapping your app, check out:
+ * http://sailsjs.org/#documentation
+ */
+
+function logUser(user, label) {
+  console.log("\n", label);
+  console.log("- username: " + user.email);
+  console.log("- password: " + user.password);
+}
+
+function logClient(client, label) {
+  console.log("\n", label);
+  console.log("- client_id: " + client.clientId);
+  console.log("- client_secret: " + client.clientSecret);
+  console.log("- redirectURI: " + client.redirectURI);
+}
+
+function createUser(theUser) {
+  // Create a user
+  User.findOne({email: theUser.email}, function(err, user) {
+    if(!user){
+      User.create(theUser).exec(function(err,user) {
+        logUser(theUser, "Default user created");
+      });
+    } else {
+      logUser(theUser, "Default user already exists");
+    }
+  });
+}
+
+function createClient(theClient) {
+  // Create a client
+  Client.findOne({name: theClient.name}, function(err, client) {
+    if(err){
+      console.log(err.message);
+    } else {
+      if(!client){
+        Client.create(theClient).exec(function(err, client) {
+          if(err){
+            console.log(err.message);
+          } else {
+            logClient(client, "The " + theClient.name + " created");
+          }
+        });
+      } else {
+        logClient(client, "The " + theClient.name + " already exists");
+      }
+    }
+  }); 
+}
+
+module.exports.bootstrap = function (cb) {
+
+  createUser({
+    email: 'adminz@gmail.com',
+    password: 'password'
+  });
+  
+  createClient({
+    name : 'trustedTestClient',
+    trusted: true,
+    redirectURI: 'http://localhost:1338'
+  });
+  
+  createClient({
+    name : 'untrustedTestClient',
+    trusted: false,
+    redirectURI: 'http://localhost:1339'
+  });
+  
+  cb();
+};
